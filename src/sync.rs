60822a372df967d71272a4f3bdd18bdaf669cf42
mbecker20 <becker.maxh@gmail.com>
2023-05-30 22:03:21 +0000

    0.1.3 remove the pointless while loops. just take first one off the iterator

--- src/sync.rs
+++ src/sync.rs
@@ -10,32 +10,27 @@ use signal_hook::{
     iterator::{exfiltrator::SignalOnly, SignalsInfo},
 };
 
-pub type ShutdownSignal = Arc<ShutdownSignalInner>; 
+pub type ShutdownSignal = Arc<ShutdownSignalInner>;
 
 pub fn term_signal_hook() -> anyhow::Result<(JoinHandle<()>, ShutdownSignal)> {
     let shutdown_signal = ShutdownSignal::default();
-    let signals = SignalsInfo::<SignalOnly>::new(&[SIGINT, SIGTERM, SIGQUIT, SIGHUP])
+    let signals = SignalsInfo::<SignalOnly>::new([SIGINT, SIGTERM, SIGQUIT, SIGHUP])
         .context("failed to initialize os signal stream")?;
     let handle = handle_signals(signals, shutdown_signal.clone());
     Ok((handle, shutdown_signal))
 }
 
-fn handle_signals(
-    mut signals: SignalsInfo,
-    shutdown_signal: ShutdownSignal,
-) -> JoinHandle<()> {
+fn handle_signals(mut signals: SignalsInfo, shutdown_signal: ShutdownSignal) -> JoinHandle<()> {
     thread::spawn(move || {
-        for signal in &mut signals {
-            println!("\nreceived terminate signal: {signal}\n");
-            shutdown_signal.trigger_shutdown();
-            loop {
-                if shutdown_signal.finished_shutdown() {
-                    println!("app finished, shutting down...");
-                    break;
-                }
-                sleep(Duration::from_millis(100));
+        let signal = signals.forever().next().expect("got None signal");
+        println!("\nreceived terminate signal: {signal}. shutting down...\n");
+        shutdown_signal.trigger_shutdown();
+        loop {
+            if shutdown_signal.finished_shutdown() {
+                println!("app finished, shutting down...");
+                break;
             }
-            break;
+            sleep(Duration::from_millis(100));
         }
     })
 }
@@ -67,18 +62,14 @@ impl ShutdownSignalInner {
 }
 
 pub fn immediate_term_handle() -> anyhow::Result<JoinHandle<()>> {
-    let signals = SignalsInfo::<SignalOnly>::new(&[SIGINT, SIGTERM, SIGQUIT, SIGHUP])
+    let signals = SignalsInfo::<SignalOnly>::new([SIGINT, SIGTERM, SIGQUIT, SIGHUP])
         .context("failed to initialize os signal stream")?;
     Ok(handle_signals_immediately(signals))
 }
 
-fn handle_signals_immediately(
-    mut signals: SignalsInfo,
-) -> JoinHandle<()> {
+fn handle_signals_immediately(mut signals: SignalsInfo) -> JoinHandle<()> {
     thread::spawn(move || {
-        for signal in &mut signals {
-            println!("\nreceived terminate signal: {signal}. shutting down...\n");
-            break;
-        }
+        let signal = signals.forever().next().expect("got None signal");
+        println!("\nreceived terminate signal: {signal}. shutting down...\n");
     })
 }
--- src/tokio.rs
+++ src/tokio.rs
@@ -10,7 +10,7 @@ pub type ShutdownSignal = Arc<ShutdownSignalInner>;
 
 pub fn term_signal_hook() -> anyhow::Result<(JoinHandle<()>, ShutdownSignal)> {
     let shutdown_signal: Arc<_> = ShutdownSignal::default();
-    let signals = Signals::new(&[SIGTERM, SIGINT, SIGQUIT, SIGHUP])
+    let signals = Signals::new([SIGTERM, SIGINT, SIGQUIT, SIGHUP])
         .context("failed to initialize os signal stream")?;
     let handle = handle_signals(signals, shutdown_signal.clone());
     Ok((handle, shutdown_signal))
@@ -18,17 +18,15 @@ pub fn term_signal_hook() -> anyhow::Result<(JoinHandle<()>, ShutdownSignal)> {
 
 fn handle_signals(mut signals: Signals, shutdown_signal: ShutdownSignal) -> JoinHandle<()> {
     tokio::spawn(async move {
-        while let Some(signal) = signals.next().await {
-            println!("\nreceived terminate signal: {signal}\n");
-            shutdown_signal.trigger_shutdown().await;
-            loop {
-                if shutdown_signal.finished_shutdown().await {
-                    println!("app finished, shutting down...");
-                    break;
-                }
-                tokio::time::sleep(Duration::from_millis(100)).await;
+        let signal = signals.next().await.expect("got None signal");
+        println!("\nreceived terminate signal: {signal}\n");
+        shutdown_signal.trigger_shutdown().await;
+        loop {
+            if shutdown_signal.finished_shutdown().await {
+                println!("app finished, shutting down...");
+                break;
             }
-            break;
+            tokio::time::sleep(Duration::from_millis(100)).await;
         }
     })
 }
@@ -60,16 +58,14 @@ impl ShutdownSignalInner {
 }
 
 pub fn immediate_term_handle() -> anyhow::Result<JoinHandle<()>> {
-    let signals = Signals::new(&[SIGTERM, SIGINT, SIGQUIT, SIGHUP])
+    let signals = Signals::new([SIGTERM, SIGINT, SIGQUIT, SIGHUP])
         .context("failed to initialize os signal stream")?;
     Ok(handle_signals_immediately(signals))
 }
 
 fn handle_signals_immediately(mut signals: Signals) -> JoinHandle<()> {
     tokio::spawn(async move {
-        while let Some(signal) = signals.next().await {
-            println!("\nreceived terminate signal: {signal}. shutting down...\n");
-            break;
-        }
+        let signal = signals.next().await.expect("got None signal");
+        println!("\nreceived terminate signal: {signal}. shutting down...\n");
     })
 }
